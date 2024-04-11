461e3165c9856b17f33980b55bfc59c2c2caaaa6
d3v3n <d3v3nx@gmail.com>
2024-04-11 09:59:35 +0530

    initial commit

---
+++ dc-forms-3e.client/src/app/components/home-page/home-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Field } from '../dynamic-form/dynamic-fields/models/field.model';
+import { BehaviorSubject, map } from 'rxjs';
+import { JsonReaderService } from '../../services/json-reader.service';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.css'
+})
+export class HomePageComponent {
+  
+}
---
+++ dc-forms-3e.client/src/app/components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.css'
+})
+export class NavBarComponent {
+  // loggedinUser :string;
+  // loggdin(){
+  //   this.loggedinUser =  localStorage.getItem('token') as string;
+  //   return this.loggedinUser;
+  // }
+  // onLogout(){
+  //   return localStorage.removeItem('token');
+
+  // }
+}
