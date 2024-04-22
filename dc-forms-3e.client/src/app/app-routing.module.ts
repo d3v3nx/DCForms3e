61abedf46e4a5090f10b47189d3052f397e2a735
d3v3n <d3v3nx@gmail.com>
2024-04-22 13:54:57 +0530

    22042024 0155pm

--- dc-forms-3e.client/src/app/app-routing.module.ts
+++ dc-forms-3e.client/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { UserLoginComponent } from './user/user-login/user-login.component';
+import { UserRegisterComponent } from './user/user-register/user-register.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'',component:HomePageComponent},
+  {path:'user/login',component:UserLoginComponent},
+  {path:'user/register',component:UserRegisterComponent},
+  {path:'**',component:HomePageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
